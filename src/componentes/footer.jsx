bec2c2a32fd09149c2d066f87ab71713735ffe2e
luizmagaroto <luiz.magaroto@estudante.ifms.edu.br>
2024-03-06 11:46:30 -0300

    atividade1

---
+++ src/componentes/footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer(){
+
+    return(
+        <Footer>
+            <p>Luiz Magaroto</p>
+        </Footer>
+    );
+
+}
+
+import Footer from "./componentes/footer";
+export default function App(){
+    return(
+        <main>
+            </Footer>
+        </main>
+    )
+}
